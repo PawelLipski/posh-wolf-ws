80f8be18b5a349cef745c2b7cb19cbce3a810d50
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-10-30 09:26:35 +0100

    Changed Web.scala to a SOAP web service

--- src/main/scala/WebOrig.scala
+++ src/main/scala/WebOrig.scala
@@ -1,5 +1,3 @@
-
-/*
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
 import com.twitter.finagle.builder.ServerBuilder
 import com.twitter.finagle.http.{Http, Response}
@@ -28,24 +26,4 @@ class Hello extends Service[HttpRequest, HttpResponse] {
     response.setContentString("Hello World")
     Future(response)
   }
-} */
-
-import javax.jws.WebService
-import javax.jws.soap.SOAPBinding
-import javax.jws.soap.SOAPBinding.Style
-import javax.xml.ws.Endpoint
- 
-@WebService(targetNamespace="org.scalabound.test", name="org.scalabound.test", portName="test", serviceName="WSTest")
-private class MinimalSoapServer {
- 
-    @SOAPBinding(style = Style.RPC)
-    def test(value : String) = "Hi " + value
- 
 }
-object Web {               
-    def main(args: Array[String]) { // main method to make this a runnable application
-        val endpoint = Endpoint.publish("http://localhost:8080", new MinimalSoapServer())
-        System.out.println("Waiting for requests...")
-    }
-}
-
