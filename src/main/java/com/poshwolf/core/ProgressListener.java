d8726059974754db4935136ecd59cd6098d215bb
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-11-11 12:15:26 +0100

    Add callback method to ProgressListener

--- src/main/java/com/poshwolf/core/ProgressListener.java
+++ src/main/java/com/poshwolf/core/ProgressListener.java
@@ -2,5 +2,9 @@ package com.poshwolf.core;
 
 public interface ProgressListener {
 
+  // TODO: set proper params for this method 
+  // (basing on the actual implemention of the cuckoo algorithm)
+  // these ones may be awkward/impossible to use
+  void onProgress(int percentDone, int resultSoFar);
 }
 
