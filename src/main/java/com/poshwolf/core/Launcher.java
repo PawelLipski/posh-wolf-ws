123a5138e04deaf42534444e316764345894e868
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-11-14 21:57:42 +0100

    Add Launcher

---
+++ src/main/java/com/poshwolf/core/Launcher.java
@@ -0,0 +1,39 @@
+
+package com.poshwolf.core;
+
+import java.util.Scanner;
+
+public class Launcher {
+
+  public static void main(String[] args) {    
+    Scanner sc = new Scanner(System.in);
+    
+    int no = 0;
+    while (sc.hasNextLine()) {      
+      sc.nextLine();
+      
+      int jobCnt = sc.nextInt();
+      
+      int machineCnt = sc.nextInt();
+      sc.nextInt();
+      int upperBound = sc.nextInt();
+      int lowerBound = sc.nextInt();          
+      sc.nextLine();
+      
+      sc.nextLine();
+      
+      int[][] opDurationsForJobs = new int[jobCnt][machineCnt];
+      
+      for (int m = 0; m < machineCnt; m++) {
+	for (int j = 0; j < jobCnt; j++) {
+	  opDurationsForJobs[j][m] = sc.nextInt();
+	  //System.out.println(opDurationsForJobs[j][m]);
+	}
+      }       
+      sc.nextLine();
+      
+      System.out.println(no + ": " + jobCnt + "/" + machineCnt);
+      no++;
+    }
+  }
+}
